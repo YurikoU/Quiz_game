9936644ce3a57647243b4b9684922fe11e1d2822
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-06-25 22:21:47 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-const question = 'What is the most sold game machine in Japan?';
-const answers = [
-    'Super Famicon',
-    'Playstation 2',
-    'Nintendo Switch',
-    'Nintendo DS'
+const quiz = [
+    {
+        question: 'What is the most sold game machine in Japan?',
+        answers: [
+            'Super Famicon',
+            'Playstation 2',
+            'Nintendo Switch',
+            'Nintendo DS'
+        ],
+        correct: 'Nintendo DS'
+    }, {
+        question: "Where is the capital of Canada?",
+        answers: [
+            'Vancouver',
+            'Toronto',
+            'Ottawa',
+            'Montreal'
+        ],
+        correct: 'Ottawa'
+    }, {
+        question: 'What is the first name of Lady Gaga?',
+        answers: [
+            'Stefani',
+            'Elizabeth',
+            'Alison',
+            'Katheryn'
+        ],
+        correct: 'Stefani'
+    }
 ];
-const correct = 'Nintendo DS';
+const quizLength = quiz.length;
+let quizIndex = 0;
+
+
+
+
 
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
@@ -14,10 +42,10 @@ const buttonLength = $button.length;
 
 //クイズの答えを、ボタンに代入する。
 const setupQuiz = () => {
-    document.getElementById('js-question').textContent = question;
+    document.getElementById('js-question').textContent = quiz[quizIndex].question;
     let buttonIndex = 0;
     while(buttonIndex < buttonLength) {
-        $button[buttonIndex].textContent = answers[buttonIndex];
+        $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
         buttonIndex++;
     };
 };
@@ -26,10 +54,18 @@ setupQuiz();
 
 //クリックされた要素のテキストが、正解と一致しているか確認。
 const clickHandler = (e) => {
-    if (correct === e.target.textContent) {
+    if (quiz[quizIndex].correct === e.target.textContent) {
         window.alert('Correct!!');
     } else {
-        window.alert('Try again :(');
+        window.alert('Oh no :(');
+    }
+    
+    quizIndex++;
+
+    if (quizIndex < quizLength) {
+        setupQuiz();
+    } else {
+        window.alert('All quizzes are done! Good job :)');
     }
 };
 
